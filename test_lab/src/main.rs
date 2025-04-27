4be8d5473b19e98998dcbeaf3c92089841824478
CHIDESKTOP1 <chihirolego1220@gmail.com>
2025-04-27 18:20:40 +0900

    Functionトレイトを実装(2.2,3)

--- test_lab/src/main.rs
+++ test_lab/src/main.rs
@@ -1,8 +1,12 @@
+//use std::any::type_name;
+
 fn main() {
-    let mut a = Variable::init(1.0);
-    println!("{}", a.data);
-    a.data = 5.0;
-    println!("{}", a.data);
+    let x = Variable::init(2.0);
+    println!("{}", x.data);
+    let f = Square {};
+    let y = f.call(&x);
+    println!("{}", y.data);
+    println!("{}", x.data);
 }
 
 struct Variable {
@@ -12,5 +16,24 @@ struct Variable {
 impl Variable {
     fn init(data: f32) -> Self {
         Variable { data }
+    }
+}
+
+trait Function {
+    fn call(&self, input: &Variable) -> Variable {
+        let x = input.data;
+        let y = self.forward(x);
+        let output = Variable::init(y);
+        output
+    }
+
+    fn forward(&self, x: f32) -> f32;
+}
+
+struct Square {}
+
+impl Function for Square {
+    fn forward(&self, x: f32) -> f32 {
+        x.powf(2.0)
     }
 }
