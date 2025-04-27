94a8ea1b11a5d3af19b22611a5c738d33948a10a
CHIDESKTOP1 <chihirolego1220@gmail.com>
2025-04-27 16:30:10 +0900

    Variableクラスの実装(1.2)

--- test_lab/src/main.rs
+++ test_lab/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let mut a = Variable::init(1.0);
+    println!("{}", a.data);
+    a.data = 5.0;
+    println!("{}", a.data);
+}
+
+struct Variable {
+    data: f32,
+}
+
+impl Variable {
+    fn init(data: f32) -> Self {
+        Variable { data }
+    }
 }
