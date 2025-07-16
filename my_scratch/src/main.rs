f9622dbba5e4b7aec5a88f5f4e64010f13403242
CHIDESKTOP1 <chihirolego1220@gmail.com>
2025-07-16 15:57:31 +0900

    GRAD_CONFIGをMutexを使って再現

--- my_scratch/src/main.rs
+++ my_scratch/src/main.rs
@@ -1,19 +1,29 @@
-use std::ops::Add;
+use once_cell::sync::Lazy; // 1.3.1
+use rand::Rng;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Mutex;
+
+static GRAD_CONFIG: Mutex<bool> = Mutex::new(true);
+
+fn set_grad_true() {
+    let mut flag = GRAD_CONFIG.lock().unwrap();
+    *flag = true;
+}
+
+fn set_grad_false() {
+    let mut flag = GRAD_CONFIG.lock().unwrap();
+    *flag = false;
+}
+
+fn get_grad_status() -> bool {
+    let flag = GRAD_CONFIG.lock().unwrap();
+    *flag
+}
 
 fn main() {
-    let iti = Variable { data: 1.0 };
-    let ni = Variable { data: 2.0 };
-    println!("{}", iti + ni);
-}
-
-#[derive(Debug, Clone)]
-struct Variable {
-    data: f32,
-}
-
-impl Add for Variable {
-    type Output = f32;
-    fn add(self, rhs: Self) -> Self::Output {
-        self.data + rhs.data
-    }
+    println!("{}", get_grad_status());
+    set_grad_false();
+    println!("{}", get_grad_status());
+    set_grad_true();
+    println!("{}", get_grad_status());
 }
