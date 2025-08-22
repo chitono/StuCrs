af68abadab9c7a194a777294cea83de6cfccd9a5
BUKKABU_GPU <buturikagakubu.gpu@gmail.com>
2025-08-22 15:01:09 +0900

    ニューラルネットワークの実装(一回微分版)

--- my_scratch/src/main.rs
+++ my_scratch/src/main.rs
@@ -1,66 +1,94 @@
 use ndarray::{array,Array, Data};
 use stucrs::core::RcVariable;
-use stucrs::functions::sum;
+use stucrs::functions::{linear_simple, sigmoid_simple, sum};
 use std::array;
 use std::time::Instant;
 
-use stucrs::functions::matmul;
+use std::f32::consts::PI;
+
+use stucrs::functions::{mean_squared_error,matmul};
 use stucrs::{ArrayDToRcVariable, F32ToRcVariable};
 
 use ndarray_rand::RandomExt;
 use ndarray_rand::rand_distr::Uniform;
 
-fn predict(x:&RcVariable,w:&RcVariable,b:&RcVariable) -> RcVariable {
-        let y = matmul(&x, &w) + b.clone();
-        y
-    }
-
-fn mean_squared_error(x0:&RcVariable,x1:&RcVariable) ->RcVariable{
-    let diff =x0.clone()-x1.clone();
-    let len = diff.len() as f32;
-    println!("len = {:?}",len);
-    
-    let error = sum(&diff.pow(2.0), None) /len.rv();
-    
-    error
 
 
-}
+
 
 fn main() {
-    let start = Instant::now();
+    
     
     let x_data = Array::random((100, 1), Uniform::new(0.0f32, 1.0));
         //let x2 = array![[11.0f32, 12.0, 13.0], [14.0, 15.0, 16.0]].rv();
-        
-        
-    let y_data = 2.0*x_data.clone() +5.0 + Array::random((100, 1), Uniform::new(0.0f32, 1.0));
-    println!("y_data = {:?}",y_data.clone());
+    
+    let y_data= x_data.mapv(|x|2.0*PI*x)+Array::random((100, 1), Uniform::new(0.0f32, 1.0));
 
     let x=x_data.rv();
 
     let y=y_data.rv();
+
+    let I =1;
+    let H = 10;
+    let O =1;
     
-    let mut w = Array::zeros((1,1)).rv();
-    let mut b = Array::zeros(1).rv();
+    let mut W1 =(0.01*Array::random((I, H), Uniform::new(0.0f32, 1.0))).rv();
+
+    let mut B1 =Array::zeros(H).rv();
+
+    let mut W2 = (0.01*Array::random((H,O), Uniform::new(0.0f32, 1.0))).rv();
+
+    let mut B2=Array::zeros(O).rv();
+
+
+    fn predict(x:&RcVariable,w1:&RcVariable,b1:&Option<RcVariable>,w2: &RcVariable,b2: &Option<RcVariable>) -> RcVariable {
+    
     
 
+        let t0 =linear_simple(&x, &w1 , &b1);
+        
+        let t1=sigmoid_simple(&t0);
+        
+        let y =linear_simple(&t1, &w2, &b2);
+
+        y
+    }
+
+
+
+
+
+
+    //let y_data = 2.0*x_data.clone() +5.0 + Array::random((100, 1), Uniform::new(0.0f32, 1.0));
+    //println!("y_data = {:?}",y_data.clone());
+
+    //let x=x_data.rv();
+
+    //let y=y_data.rv();
     
+    //let mut w = Array::zeros((1,1)).rv();
+    //let mut b = Array::zeros(1).rv();
+    
+
+    let start = Instant::now();
     //set_grad_false();
-    let lr =0.1;
+    let lr =0.2;
     let iters = 10000;
-    for _i in 0..iters {
+    for i in 0..iters {
         //println!("w1 = {:?}\n", w.clone());
 
-       let y_pred=predict(&x, &w, &b);
+       let y_pred=predict(&x , &W1, &Some(B1.clone()), &W2, &Some(B2.clone()));
        //println!("y_pred = {:?}\n",y_pred.clone().data().clone());
         //println!("w2 = {:?}\n", w.clone());
-
+        
        let mut loss=mean_squared_error(&y, &y_pred);
+       
         //println!("loss = {:?}\n", loss.0.borrow().creator);
 
-       w.cleargrad();
-       b.cleargrad();
+       W1.cleargrad();
+       W2.cleargrad();
+       B1.cleargrad();
+       B2.cleargrad();
        
        loss.backward();
        //println!("wwwwwwwwwwwwwwwwwwwwwwwwwwwww\n");
@@ -71,29 +99,33 @@ fn main() {
        //let b_data=lr*b.grad();
 
 
-       let w_data=w.data().clone();
-       let b_data=b.data().clone();
-
+       let w1_data=W1.data();
+       let b1_data=B1.data();
+       let w2_data=W2.data();
+       let b2_data=B2.data();
        //let w.data=w_data-lr*w.grad(); 
 
   
 
-        let current_grad_w =w.grad().unwrap();
-        let current_grad_b =b.grad().unwrap();
-
+        let current_grad_w1 =W1.grad().unwrap();
+        let current_grad_b1 =B1.grad().unwrap();
+        let current_grad_w2 =W2.grad().unwrap();
+        let current_grad_b2 =B2.grad().unwrap();
        // w =  (w_data - lr*current_grad_w).rv();
        // b = (b_data - lr*current_grad_b).rv();
 
-        w.0.borrow_mut().data =w_data- lr*current_grad_w;
-        b.0.borrow_mut().data = b_data- lr*current_grad_b;
-
-    
+        W1.0.borrow_mut().data =w1_data- lr*current_grad_w1;
+        B1.0.borrow_mut().data = b1_data- lr*current_grad_b1;
+        W2.0.borrow_mut().data =w2_data- lr*current_grad_w2;
+        B2.0.borrow_mut().data = b2_data- lr*current_grad_b2;
 
     // y.backward();
+        if i%1000 == 0 {
+            println!("loss = {:?}\n", loss.data());
+        }
+        
 
-        println!("w = {:?}\n", w.clone().data());
-
-        println!("b= {:?}\n", b.clone().data());
+        //println!("b= {:?}\n", b.clone().data());
         //println!("loss = {:?}\n", loss.clone());
         //println!("x1_grad = {:?}\n", x1.clone().grad());
 
