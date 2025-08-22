0bc727833101dc28c04813fafca8000203a1a0e1
BUKKABU_GPU <buturikagakubu.gpu@gmail.com>
2025-08-22 10:17:08 +0900

    F32ToRcVariableのトレイトを搭載する型をf64からf32に変更

--- stucrs/src/lib.rs
+++ stucrs/src/lib.rs
@@ -6,40 +6,43 @@ use std::ops::{Add, Div, Mul, Neg, Sub};
 
 //演算子のオーバーロード
 
-impl Add for &RcVariable {
+impl Add for RcVariable {
     type Output = RcVariable;
-    fn add(self, rhs: &RcVariable) -> Self::Output {
+    fn add(self, rhs: RcVariable) -> Self::Output {
         // add_op関数はRc<RefCell<Variable>>を扱う
         let add_y = add(&[Some(self.0.clone()), Some(rhs.0.clone())]);
         RcVariable(add_y.clone())
     }
 }
 
-impl Mul for &RcVariable {
+
+
+impl Mul for RcVariable {
     type Output = RcVariable;
-    fn mul(self, rhs: &RcVariable) -> Self::Output {
+    fn mul(self, rhs: RcVariable) -> Self::Output {
         let mul_y = mul(&[Some(self.0.clone()), Some(rhs.0.clone())]);
         RcVariable(mul_y.clone())
     }
 }
 
-impl Sub for &RcVariable {
+
+impl Sub for RcVariable {
     type Output = RcVariable;
-    fn sub(self, rhs: &RcVariable) -> Self::Output {
+    fn sub(self, rhs: RcVariable) -> Self::Output {
         let sub_y = sub(&[Some(self.0.clone()), Some(rhs.0.clone())]);
         RcVariable(sub_y.clone())
     }
 }
 
-impl Div for &RcVariable {
+impl Div for RcVariable {
     type Output = RcVariable;
-    fn div(self, rhs: &RcVariable) -> Self::Output {
+    fn div(self, rhs: RcVariable) -> Self::Output {
         let div_y = div(&[Some(self.0.clone()), Some(rhs.0.clone())]);
         RcVariable(div_y.clone())
     }
 }
 
-impl Neg for &RcVariable {
+impl Neg for RcVariable {
     type Output = RcVariable;
     fn neg(self) -> Self::Output {
         let neg_y = neg(&[Some(self.0.clone()), None]);
@@ -64,9 +67,9 @@ pub trait F32ToRcVariable {
 
 //rustの数値のデフォルトがf64なので、f32に変換する
 //f32からarray型に変換し、rv()でRcVariableを生成
-impl F32ToRcVariable for f64 {
+impl F32ToRcVariable for f32 {
     fn rv(&self) -> RcVariable {
-        let array = array![*self as f32];
+        let array = array![*self];
         array.rv()
     }
 }
