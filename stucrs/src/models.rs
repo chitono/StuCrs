fc3193ac5806ee6fd80c267b3527c71d578ffd93
CHIDESKTOP1 <chihirolego1220@gmail.com>
2025-08-26 19:03:09 +0900

    modelがlayerをRc<Refcell>で包まれた状態で保持するよう変更(layerをoptimizerと共同で持つため)

--- stucrs/src/models.rs
+++ stucrs/src/models.rs
@@ -7,35 +7,35 @@ use std::rc::{Rc, Weak};
 pub trait Model {
     fn stack(&mut self, layer: impl Layer + 'static);
     fn forward(&mut self, x: &RcVariable) -> RcVariable;
-    fn layers(&mut self) -> &mut Vec<Box<dyn Layer + 'static>>;
+    fn layers(&mut self) -> Rc<RefCell<Vec<Box<dyn Layer + 'static>>>>;
     fn cleargrad(&mut self);
 }
 
 pub struct BaseModel {
     input: Option<Weak<RefCell<Variable>>>,
     output: Option<Weak<RefCell<Variable>>>,
-    layers: Vec<Box<dyn Layer + 'static>>,
+    layers: Rc<RefCell<Vec<Box<dyn Layer + 'static>>>>,
 }
 
 impl Model for BaseModel {
     fn stack(&mut self, layer: impl Layer + 'static) {
         let add_layer = Box::new(layer);
-        self.layers.push(add_layer);
+        self.layers.borrow_mut().push(add_layer);
     }
 
     fn cleargrad(&mut self) {
-        for layer in self.layers.iter_mut() {
+        for layer in self.layers.borrow_mut().iter_mut() {
             layer.cleargrad();
         }
     }
-    fn layers(&mut self) -> &mut Vec<Box<dyn Layer + 'static>> {
-        &mut self.layers
+    fn layers(&mut self) -> Rc<RefCell<Vec<Box<dyn Layer + 'static>>>> {
+        self.layers.clone()
     }
 
     fn forward(&mut self, x: &RcVariable) -> RcVariable {
         let mut y = x.clone();
 
-        for layer in self.layers.iter_mut() {
+        for layer in self.layers.borrow_mut().iter_mut() {
             let t = y;
             y = layer.call(&t);
         }
@@ -49,7 +49,7 @@ impl BaseModel {
         BaseModel {
             input: None,
             output: None,
-            layers: Vec::new(),
+            layers: Rc::new(RefCell::new(Vec::new())),
         }
     }
 
