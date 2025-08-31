2da024bc988ff52c8ac0f2e1ad2bc487c950510c
CHIDESKTOP1 <chihirolego1220@gmail.com>
2025-08-31 19:27:10 +0900

    スパイラルデータの学習(48.2)

--- my_scratch/src/main.rs
+++ my_scratch/src/main.rs
@@ -6,7 +6,7 @@ use std::rc::Rc;
 use std::time::Instant;
 use stucrs::core_new::{F32ToRcVariable, RcVariable};
 use stucrs::datasets::*;
-use stucrs::functions_new as F;
+use stucrs::functions_new::{self as F, sum};
 use stucrs::layers::{self as L, Activation, Dense, Layer, Linear};
 use stucrs::models::{BaseModel, Model};
 use stucrs::optimizers::{Optimizer, SGD};
@@ -20,19 +20,65 @@ use plotters::prelude::*;
 use rand::seq::SliceRandom;
 use rand::*;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
+    let max_epoch = 300;
+    let lr = 1.0;
+    let batch_size = 30;
     // 3x4の2次元行列を作成
     // `mut`キーワードで可変にする
-    let spiral = Spiral::new(true);
+    let mut spiral = Spiral::new(true);
 
-    let t = spiral.label;
     let x = spiral.data.view();
+    let t = spiral.label.view_mut();
+    let t = to_one_hot(t, 3);
 
-    for (i, batch) in x.axis_chunks_iter(Axis(0), 10).enumerate() {
-        println!("i = {:?} batch = {:?}", i, batch);
+    let mut model = BaseModel::new();
+    model.stack(L::Dense::new(10, true, None, Activation::Sigmoid));
+    model.stack(L::Linear::new(3, true, None));
+
+    let data_size = x.shape()[0];
+    let mut optimizer = SGD::new(lr);
+    optimizer.setup(&model);
+    let start = Instant::now();
+    for epoch in 0..max_epoch {
+        let mut indices: Vec<usize> = (0..x.shape()[0]).collect();
+        let mut rng = thread_rng();
+        indices.shuffle(&mut rng);
+        let mut sum_loss = array![0.0f32];
+
+        for chunk_indices in indices.chunks(batch_size) {
+            let x_batch = x.select(Axis(0), chunk_indices).rv();
+            let t_batch = t.select(Axis(0), chunk_indices).rv();
+
+            //println!("x_batch = {:?}, t_batch = {:?}", x_batch, t_batch);
+
+            let y = model.call(&x_batch);
+            let mut loss = F::softmax_cross_entropy_simple(&y, &t_batch);
+            model.cleargrad();
+            loss.backward(false);
+            optimizer.update();
+
+            //ここでt_batch.lenはu32からf32に変換、さらに暗黙的にndarray型に変換されて、計算される。
+            //また、sum_lossは静的次元なので、epoch_lossを動的次元から静的次元に変換して足せるようにする。
+
+            let epoch_loss: Array1<f32> = (&loss.data() * (t_batch.len() as f32))
+                .into_dimensionality()
+                .unwrap();
+
+            sum_loss = &sum_loss + &epoch_loss;
+        }
+
+        let average_loss = &sum_loss / (data_size as f32);
+
+        println!("epoch = {:?}, loss = {:?}", epoch + 1, average_loss);
     }
+    let end = Instant::now();
+    let duration = end.duration_since(start);
+    println!("処理時間{:?}", duration);
+}
 
-    let root = BitMapBackend::new("plot.png", (640, 640)).into_drawing_area();
+/*
+let root = BitMapBackend::new("plot.png", (640, 640)).into_drawing_area();
     root.fill(&WHITE)?;
     let mut chart = ChartBuilder::on(&root)
         .margin(10)
@@ -58,4 +104,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     )?;
 
     Ok(())
-}
+
+    for (x_batch,t_batch) in x.axis_chunks_iter(Axis(0), 10),t.axis_iter(Axis()) {
+        println!("i = {:?} batch = {:?}", i, batch);
+
+*/
