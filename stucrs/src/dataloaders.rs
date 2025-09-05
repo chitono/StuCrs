0f249413b04029de50c8dfdf3de7ecbc40ad9f7b
CHIDESKTOP1 <chihirolego1220@gmail.com>
2025-09-05 15:33:14 +0900

    dataloaderの基礎を実装

--- stucrs/src/dataloaders.rs
+++ stucrs/src/dataloaders.rs
@@ -1,1 +1,36 @@
+use ndarray::*;
+use rand;
+use rand::seq::{index, SliceRandom};
 
+struct DataLoader {
+    dataset: ArrayD<f32>,
+    batch_size: usize,
+    shuffle: bool,
+    data_size: usize,
+    indices: Vec<usize>,
+}
+
+impl Iterator for DataLoader {
+    fn next(&mut self) -> Option<Self::Item> {}
+}
+
+impl DataLoader {
+    fn new(datasets: ArrayD<f32>, batch_size: usize, shuffle: bool) -> Self {
+        let x_len = datasets.view().shape()[0];
+        let mut indices: Vec<usize>;
+        if shuffle == true {
+            indices = (0..x_len).collect();
+            indices.shuffle(&mut rand::thread_rng());
+        } else {
+            indices = (0..x_len).collect();
+        }
+
+        Self {
+            dataset: datasets,
+            batch_size: batch_size,
+            shuffle: shuffle,
+            data_size: x_len,
+            indices: indices,
+        }
+    }
+}
