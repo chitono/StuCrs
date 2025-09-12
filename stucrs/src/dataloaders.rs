9df9b247c86c87d9c75e2141f3e70e73e0d243fa
BUKKABU_GPU <buturikagakubu.gpu@gmail.com>
2025-09-12 15:57:09 +0900

    dataloader微調整(返すところまだ)

--- stucrs/src/dataloaders.rs
+++ stucrs/src/dataloaders.rs
@@ -2,21 +2,37 @@ use ndarray::*;
 use rand;
 use rand::seq::{index, SliceRandom};
 
+use crate::core_new::ArrayDToRcVariable;
+
 struct DataLoader {
-    dataset: ArrayD<f32>,
+    x_data_set: ArrayD<f32>,
+    t_data_set: ArrayD<f32>,
     batch_size: usize,
     shuffle: bool,
     data_size: usize,
     indices: Vec<usize>,
+    count: usize,
+    max_count: usize,
 }
+
+
 
 impl Iterator for DataLoader {
-    fn next(&mut self) -> Option<Self::Item> {}
-}
+    fn next(&mut self) -> Option<Self::Item> {
+        for chunk_indices in self.indices.chunks(self.batch_size) {
+            let x_batch =self.x_data_set.select(Axis(0), chunk_indices).rv();
+            let t_batch =self.t_data_set.select(Axis(0), chunk_indices).rv();
+            (x_batch,t_batch)
+        }
+        
 
+    }
+}
+ 
 impl DataLoader {
-    fn new(datasets: ArrayD<f32>, batch_size: usize, shuffle: bool) -> Self {
-        let x_len = datasets.view().shape()[0];
+    fn new(x_data_set: ArrayD<f32>,t_data_set:ArrayD<f32>, batch_size: usize, shuffle: bool) -> Self {
+        let x_len = x_data_set.view().shape()[0];
+        let max_count = ((x_len/batch_size) as f32).ceil();
         let mut indices: Vec<usize>;
         if shuffle == true {
             indices = (0..x_len).collect();
@@ -26,11 +42,15 @@ impl DataLoader {
         }
 
         Self {
-            dataset: datasets,
+            x_data_set: x_data_set,
+            t_data_set: t_data_set,
             batch_size: batch_size,
             shuffle: shuffle,
             data_size: x_len,
             indices: indices,
+            count:0,
+            max_count:max_count as usize,
         }
     }
 }
+
