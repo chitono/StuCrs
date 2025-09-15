20538a3b0ad337536f2ca3ba854be16ba8f8686a
CHIDESKTOP1 <chihirolego1220@gmail.com>
2025-09-15 10:24:13 +0900

    dataloaderの実装は一旦保留

--- stucrs/src/dataloaders.rs
+++ stucrs/src/dataloaders.rs
@@ -2,7 +2,7 @@ use ndarray::*;
 use rand;
 use rand::seq::{index, SliceRandom};
 
-use crate::core_new::ArrayDToRcVariable;
+use crate::core_new::{ArrayDToRcVariable, RcVariable};
 
 struct DataLoader {
     x_data_set: ArrayD<f32>,
@@ -10,35 +10,44 @@ struct DataLoader {
     batch_size: usize,
     shuffle: bool,
     data_size: usize,
-    indices: Vec<usize>,
+    index: Vec<usize>,
     count: usize,
     max_count: usize,
 }
 
-
+/*
 
 impl Iterator for DataLoader {
+    type Item = (RcVariable, RcVariable);
     fn next(&mut self) -> Option<Self::Item> {
-        for chunk_indices in self.indices.chunks(self.batch_size) {
-            let x_batch =self.x_data_set.select(Axis(0), chunk_indices).rv();
-            let t_batch =self.t_data_set.select(Axis(0), chunk_indices).rv();
-            (x_batch,t_batch)
+        if self.count >= self.max_count {
+            None
+        } else {
+            let i = self.count;
+            for chunk_indices in self.index.chunks(self.batch_size) {
+                let x_batch = self.x_data_set.select(Axis(0), chunk_indices).rv();
+                let t_batch = self.t_data_set.select(Axis(0), chunk_indices).rv();
+            }
         }
-        
-
     }
 }
- 
+
+*/
 impl DataLoader {
-    fn new(x_data_set: ArrayD<f32>,t_data_set:ArrayD<f32>, batch_size: usize, shuffle: bool) -> Self {
+    fn new(
+        x_data_set: ArrayD<f32>,
+        t_data_set: ArrayD<f32>,
+        batch_size: usize,
+        shuffle: bool,
+    ) -> Self {
         let x_len = x_data_set.view().shape()[0];
-        let max_count = ((x_len/batch_size) as f32).ceil();
-        let mut indices: Vec<usize>;
+        let max_count = ((x_len / batch_size) as f32).ceil();
+        let mut index: Vec<usize>;
         if shuffle == true {
-            indices = (0..x_len).collect();
-            indices.shuffle(&mut rand::thread_rng());
+            index = (0..x_len).collect();
+            index.shuffle(&mut rand::thread_rng());
         } else {
-            indices = (0..x_len).collect();
+            index = (0..x_len).collect();
         }
 
         Self {
@@ -47,10 +56,9 @@ impl DataLoader {
             batch_size: batch_size,
             shuffle: shuffle,
             data_size: x_len,
-            indices: indices,
-            count:0,
-            max_count:max_count as usize,
+            index: index,
+            count: 0,
+            max_count: max_count as usize,
         }
     }
 }
-
