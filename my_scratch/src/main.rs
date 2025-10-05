9152bb7cd708ae4698542174761168f657e0d7ba
CHIDESKTOP1 <chihirolego1220@gmail.com>
2025-10-05 18:59:55 +0900

    tensor_frameのsum_axisを実装

--- my_scratch/src/main.rs
+++ my_scratch/src/main.rs
@@ -12,6 +12,7 @@ use tensor_frame::*;
 //use std::f32::consts::PI;
 //use std::rc::Rc;
 use std::time::Instant;
+use std::vec;
 use stucrs_gpu::config;
 
 //use stucrs::core_new::{F32ToRcVariable, RcVariable};
@@ -21,127 +22,17 @@ use stucrs_gpu::functions_new::{self as F, accuracy};
 use stucrs_gpu::layers::{self as L, Activation};
 use stucrs_gpu::models::{BaseModel, Model};
 use stucrs_gpu::optimizers::{Optimizer, SGD};
+use tensor_frame::{Shape, Tensor, TensorOps};
 
 fn main() {
-    let mnist = MNIST::new();
-    let x_train = mnist.train_img.view();
-    let y_train = mnist.train_label.view();
-    let x_test = mnist.test_img.view();
-    let y_test = mnist.test_label.view();
+    let a = Tensor::from_vec(vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0], vec![2, 3]).unwrap();
+    let b = a.sum(Some(1)).unwrap();
 
-    //let image_num = 0;
-
-    //println!("{:#.1?}\n", mnist.get_item(image_num));
-
-    //println!("{:?}", x_train.shape());
-
-    //println!("{:?}", y_train.shape());
-
-    let x_train = x_train.to_shape((50000, 28 * 28)).unwrap();
-    let x_test = x_test.to_shape((10000, 28 * 28)).unwrap();
-
-    let y_train = arr2d_to_one_hot(y_train.mapv(|x| x as u32).view(), 10);
-    let y_test = arr2d_to_one_hot(y_test.mapv(|x| x as u32).view(), 10);
-
-    let x_train = Tensor::from_vec(x_train.clone().into_iter().collect(), x_train.shape()).unwrap();
-    let x_test = Tensor::from_vec(x_test.clone().into_iter().collect(), x_test.shape()).unwrap();
-    let y_train = Tensor::from_vec(y_train.clone().into_iter().collect(), y_train.shape()).unwrap();
-    let y_test = Tensor::from_vec(y_test.clone().into_iter().collect(), y_test.shape()).unwrap();
-
-    println!("{:?}", x_train.backend_type());
-
-    //println!("faltten = {:?}", x_train.shape());
-    //println!("faltten = {:?}", x_test.shape());
-
-    //println!("y_train = {:?}", y_train);
-
-    let max_epoch = 1;
-    let lr = 0.01;
-    let batch_size = 100;
-
-    // 3x4の2次元行列を作成
-    // `mut`キーワードで可変にする
-    /*
-    let train_spiral = Spiral::new(true);
-    let test_spiral = Spiral::new(true);
-
-    let x_train = train_spiral.data;
-    let y_train = train_spiral.label.view();
-    let y_train = to_one_hot(y_train, 3);
-
-    let x_test = test_spiral.data;
-    let y_test = test_spiral.label.view();
-    let y_test = to_one_hot(y_test, 3);
-    */
-
-    let data_size = x_train.shape().dims()[0];
-    println!("data_size={}", data_size);
-
-    let mut model = BaseModel::new();
-    model.stack(L::Dense::new(1000, true, None, Activation::Sigmoid));
-
-    model.stack(L::Linear::new(10, true, None));
-
-    let mut optimizer = SGD::new(lr);
-    optimizer.setup(&model);
-    let start = Instant::now();
-    for epoch in 0..max_epoch {
-        let mut indices: Vec<usize> = (0..data_size).collect();
-        let mut rng = thread_rng();
-        indices.shuffle(&mut rng);
-
-        let mut sum_loss = array![0.0f32];
-        let mut sum_acc = 0.0f32;
-
-        /*
-        let train_loader = DataLoader::new(
-            x_train.clone().into_dyn(),
-            y_train.clone().into_dyn(),
-            batch_size,
-            true,
-        );
-        let test_loader = DataLoader::new(
-            x_test.clone().into_dyn(),
-            y_test.clone().into_dyn(),
-            batch_size,
-            true,
-        ); */
-
-        //for (x_batch, y_batch) in train_loader {
-        //println!("x_batch = {:?}, t_batch = {:?}", x_batch, t_batch);
-
-        let y = model.call(&x_train.rv());
-        println!("y ={:?}", y.data().backend_type());
-
-        let mut loss = F::softmax_cross_entropy_simple(&y, &y_train.rv());
-        println!("loss ={:?}", loss.data().backend_type());
-
-        /*
-        let acc = accuracy(
-            y.data().into_dimensionality().unwrap().view(),
-            y_batch.data().into_dimensionality().unwrap().view(),
-        ); */
-        model.cleargrad();
-        println!("1");
-        loss.backward(false);
-        println!("2");
-        optimizer.update();
-        println!("3");
-
-        //ここでt_batch.lenはu32からf32に変換、さらに暗黙的にndarray型に変換されて、計算される。
-        //また、sum_lossは静的次元なので、epoch_lossを動的次元から静的次元に変換して足せるようにする。
-
-        //sum_acc = sum_acc + acc * (y_batch.len() as f32);
-
-        //let average_acc = sum_acc / (data_size as f32);
-
-        println!("epoch = {:?}, train_loss = {:?}", epoch + 1, loss.data(),);
-
-        config::set_grad_true();
-    }
-    let end = Instant::now();
-    let duration = end.duration_since(start);
-    println!("処理時間{:?}", duration);
+    println!(
+        "b = {:?}, shape = {:?}",
+        b.to_vec().to_owned(),
+        b.shape().dims()
+    );
 }
 
 /*
