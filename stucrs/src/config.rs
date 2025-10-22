cf7bf8f96ababb433f772f557968b31bdb94e863
BUKKABU_GPU <buturikagakubu.gpu@gmail.com>
2025-10-22 16:53:01 +0900

    cpu版でTEST_FLAGを実装

--- stucrs/src/config.rs
+++ stucrs/src/config.rs
@@ -6,6 +6,7 @@ pub static NEXT_ID: AtomicUsize = AtomicUsize::new(0);
 /// 推論するときなど、微分する必要がないときに切り替える
 pub static GRAD_CONFIG: AtomicBool = AtomicBool::new(true);
 
+pub static TEST_FLAG: AtomicBool = AtomicBool::new(false);
 /// idを生成する関数。構造体のコンストラクタを作成する際に、呼び出して、idを付ける
 pub fn id_generator() -> usize {
     NEXT_ID.fetch_add(1, Ordering::SeqCst)
@@ -22,3 +23,20 @@ pub fn set_grad_false() {
 pub fn get_grad_status() -> bool {
     GRAD_CONFIG.load(Ordering::SeqCst)
 }
+
+
+
+
+/// test_flagをtrueに変更する関数。
+pub fn set_test_flag_true() {
+    TEST_FLAG.store(true, Ordering::SeqCst);
+}
+
+pub fn set_test_flag_false() {
+    TEST_FLAG.store(false, Ordering::SeqCst);
+}
+
+pub fn get_test_flag_status() -> bool {
+    TEST_FLAG.load(Ordering::SeqCst)
+}
+
