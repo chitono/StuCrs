a2fcd23fc66a568bde283d08e6fb804b069d9205
CHIDESKTOP1 <chihirolego1220@gmail.com>
2026-02-21 17:00:05 +0900

    CNNでのMNIST学習コード(処理が遅い)

--- stucrs_test/src/main.rs
+++ stucrs_test/src/main.rs
@@ -1,22 +1,15 @@
-use image::{GrayImage, Luma};
 use ndarray::*;
-use ndarray_stats::QuantileExt;
-use plotters::prelude::*;
 use rand::seq::SliceRandom;
 use rand::*;
-use std::array;
-use std::cell::RefCell;
-use std::f32::consts::PI;
-use std::rc::Rc;
 use std::time::Instant;
 use stucrs::config;
 use stucrs::core_new::ArrayDToRcVariable;
-use stucrs::core_new::{F32ToRcVariable, RcVariable};
-use stucrs::dataloaders::DataLoader;
 use stucrs::datasets::*;
 use stucrs::functions::loss::softmax_cross_entropy_simple;
 use stucrs::functions::neural_funcs::accuracy;
-use stucrs::layers::{self as L, Activation, Dense, Layer, Linear};
+use stucrs::layers::{
+    Activation, ActivationLayer, Conv2d, Dense, Dropout, Flatten, Linear, Maxpool2d,
+};
 use stucrs::models::{BaseModel, Model};
 use stucrs::optimizers::{Optimizer, SGD};
 
@@ -27,16 +20,13 @@ fn main() {
     let x_test = mnist.test_img.view();
     let y_test = mnist.test_label.view();
 
-    let image_num = 0;
+    let _image_num = 0;
 
     //println!("{:#.1?}\n", mnist.get_item(image_num));
 
     //println!("{:?}", x_train.shape());
 
     //println!("{:?}", y_train.shape());
-
-    let x_train = x_train.to_shape((50000, 28 * 28)).unwrap();
-    let x_test = x_test.to_shape((10000, 28 * 28)).unwrap();
 
     let y_train = arr2d_to_one_hot(y_train.mapv(|x| x as u32).view(), 10);
     let y_test = arr2d_to_one_hot(y_test.mapv(|x| x as u32).view(), 10);
@@ -46,9 +36,9 @@ fn main() {
 
     //println!("y_train = {:?}", y_train);
 
-    let max_epoch = 5;
+    let max_epoch = 1;
     let lr = 0.01;
-    let batch_size = 100;
+    let batch_size = 1000;
 
     // 3x4の2次元行列を作成
     // `mut`キーワードで可変にする
@@ -69,12 +59,27 @@ fn main() {
     println!("data_size={}", data_size);
 
     let mut model = BaseModel::new();
-    model.stack(L::Dense::new(1000, true, None, Activation::Sigmoid));
-    model.stack(L::Linear::new(10, true, None));
+    model.stack(Conv2d::new(32, (5, 5), (1, 1), (0, 0), false));
+    model.stack(ActivationLayer::new(Activation::Relu));
+    model.stack(Conv2d::new(32, (5, 5), (1, 1), (0, 0), false));
+    model.stack(ActivationLayer::new(Activation::Relu));
+    model.stack(Maxpool2d::new((2, 2), (1, 1), (0, 0)));
+
+    model.stack(Conv2d::new(64, (3, 3), (1, 1), (0, 0), false));
+    model.stack(ActivationLayer::new(Activation::Relu));
+    model.stack(Conv2d::new(64, (3, 3), (1, 1), (0, 0), false));
+    model.stack(ActivationLayer::new(Activation::Relu));
+    model.stack(Maxpool2d::new((2, 2), (2, 2), (0, 0)));
+
+    model.stack(Flatten::new());
+
+    model.stack(Dense::new(256, true, None, Activation::Relu));
+    model.stack(Linear::new(10, true, None));
 
     let mut optimizer = SGD::new(lr);
     optimizer.setup(&model);
     let start = Instant::now();
+
     for epoch in 0..max_epoch {
         let mut indices: Vec<usize> = (0..data_size).collect();
         let mut rng = thread_rng();
@@ -105,17 +110,18 @@ fn main() {
             //println!("x_batch = {:?}, t_batch = {:?}", x_batch, t_batch);
 
             let y = model.call(&x_batch);
-
+            println!("1");
             let mut loss = softmax_cross_entropy_simple(&y, &y_batch);
-
-            println!("loss = {:?}", loss.data());
+            println!("2");
             let acc = accuracy(
                 y.data().into_dimensionality().unwrap().view(),
                 y_batch.data().into_dimensionality().unwrap().view(),
             );
             model.cleargrad();
             loss.backward(false);
+            println!("3");
             optimizer.update();
+            println!("4");
 
             //ここでt_batch.lenはu32からf32に変換、さらに暗黙的にndarray型に変換されて、計算される。
             //また、sum_lossは静的次元なので、epoch_lossを動的次元から静的次元に変換して足せるようにする。
